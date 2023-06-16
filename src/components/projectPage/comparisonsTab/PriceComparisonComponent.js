f32a017fc69c8b8a3d216b3eab8c4b801570ab12
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-06-16 10:26:44 +0530

    comparisons tab and negotiations tab

---
+++ src/components/projectPage/comparisonsTab/PriceComparisonComponent.js
@@ -0,0 +1,10 @@
+import comparison from '../../../assets/images/comparison.png'
+
+const PriceComparisonComponent = () => {
+  return (
+    <div className="price-comparison">
+      <img src={comparison} alt="price comparison" />
+    </div>
+  )
+}
+export default PriceComparisonComponent
---
+++ src/components/projectPage/negotiationsTab/NegotiationsComponent.js
@@ -0,0 +1,13 @@
+import negotiations from '../../../assets/images/negotiations.png'
+import styled from 'styled-components'
+
+const NegotiationsComponent = () => {
+  return (
+    <div className="negotiations-component">
+      <img src={negotiations} alt="negotiations" />
+    </div>
+  )
+}
+export default NegotiationsComponent
+
+const Wrapper = styled.div``
